0526959af62f83407c5b2dfa237c02813978b50c
bfs15 <bruno.serbena@gmail.com>
2019-05-08 18:42:21 -0300

    tidy up files
    Signed-off-by: bfs15 <bruno.serbena@gmail.com>

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 import load
 import enhance
+import fingerprint
 
 from PIL import Image
 import numpy as np
@@ -21,26 +22,25 @@ if __name__ == "__main__":
     for image in images[0:]:
         image = enhance.contrast(image)
         image = enhance.median_filter(image, 5)
-
-        orientation_blocks = enhance.gradient(image, block_sz)
-
-        orientation_blocks = enhance.smooth_gradient(
-            orientation_blocks, block_sz)
-
-        image_draw = enhance.draw_orientation_map(
-            image, orientation_blocks, block_sz)
-        cv2.imshow("draw_orientation_map", image_draw)
-
+        image_bin = enhance.binarize(image, block_sz)
         image_roi = enhance.region_of_interest(image, block_sz)
 
-        poincare, s_type = enhance.singular_type(image_roi, orientation_blocks, block_sz)
+        orientation_blocks = fingerprint.gradient(image, block_sz)
 
-        image_draw = enhance.draw_singular_points(image_roi, poincare, block_sz)
+        orientation_blocks = fingerprint.smooth_gradient(
+            orientation_blocks, block_sz)
 
-        image_bin = enhance.binarize(image, block_sz)
+        image_draw = fingerprint.draw_orientation_map(
+            image_roi, orientation_blocks, block_sz)
 
+        poincare, s_type = fingerprint.singular_type(
+            image_roi, orientation_blocks, block_sz)
+
+        image_draw = fingerprint.draw_singular_points(
+            image_draw, poincare, block_sz)
+
+        cv2.imshow("image_draw", image_draw)
         cv2.imshow("image_bin", image_bin)
-        cv2.imshow("draw_singular_points", image_draw)
         cv2.waitKey(0)
         
         images_enhanced.append(image)
