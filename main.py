a43dc230d9aa59435abe68975a2ca9ce7d934f44
bfs15 <bruno.serbena@gmail.com>
2019-05-13 21:33:23 -0300

    added template to list
    Signed-off-by: bfs15 <bruno.serbena@gmail.com>

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ if __name__ == "__main__":
     
     images = load.fingerprints("DB/Rindex28")
 
-    images_enhanced = []
+    images_templated = []
     blk_sz = 11
 
     for image in images[0:]:
@@ -27,9 +27,10 @@ if __name__ == "__main__":
         image = enhance.contrast(image)
         image = enhance.median_filter(image, 5)
         image_bin = enhance.binarize(image)
-        cv2.imshow("image_bin", image_bin)
+        # cv2.imshow("image_bin", image_bin)
         image_spook = np.where(image_bin < 255, 1, 0).astype('uint8')
         image_smoothed = enhance.smooth_bin(image_spook, blk_sz)
+        # cv2.imshow("image_smoothed", image_smoothed*255)
         image_spook = skeletonize(image_smoothed).astype('uint8')
 
         image_roi, roi_blks = enhance.region_of_interest(image, blk_sz)
@@ -52,17 +53,11 @@ if __name__ == "__main__":
         image_draw = fingerprint.draw_singular_points(
             image_draw, s_type, poincare, blk_sz)
 
-        print("minutiae_list")
-        print(minutiae_list)
-        print("roi_blks.shape")
-        print(roi_blks.shape)
-
-        cv2.imshow("image_draw", image_draw)
-        cv2.imshow("image_smoothed", image_smoothed*255)
+        # cv2.imshow("image_draw", image_draw)
         cv2.imshow("image_spook", image_spook)
         cv2.waitKey(0)
         
-        images_enhanced.append(image)
+        images_templated.append(image_spook, s_type, minutiae_list)
 
         # draw_orientation_map and cv2.imshow together bug out
         # cv2.imshow("img", image)
