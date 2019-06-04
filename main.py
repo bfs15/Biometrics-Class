3b87ea896af2e102eaad492a480685d466ff24dd
bfs15 <bruno.serbena@gmail.com>
2019-06-03 22:32:50 -0300

    pyramid numba
    Signed-off-by: bfs15 <bruno.serbena@gmail.com>

--- main.py
+++ main.py
@@ -15,6 +15,10 @@ import cv2
 import sys
 import matplotlib.cm
 import time
+import itertools
+import random
+
+from numba import jit
 
 from skimage.feature import hog
 from skimage import data, exposure
@@ -26,17 +30,13 @@ verbose = True
 
 
 def sobel_filter(img, axis):
-	img = img.astype(np.float)
-
-	if axis == 0:
-			# x axis
-		kernel = np.array([[1, 0, -1]], dtype=np.float)
-	elif axis == 1:
-			# y axis
-		kernel = np.array([[1], [0], [-1]], dtype=np.float)
+	kernel = [
+		np.array([[1, 0, -1]], dtype=np.float),
+		np.array([[1], [0], [-1]], dtype=np.float)
+	]
 
 	convolved = scipy.signal.convolve2d(
-				img, kernel, mode='same', boundary='symm')
+            img, kernel[axis], mode='same', boundary='symm')
 
 	return convolved
 
@@ -46,11 +46,11 @@ def gradient(img):
 	return mag, angle
 	"""
 	#
-	# gx = sobel_filter(img, 0)
-	# gy = sobel_filter(img, 1)
+	gx = sobel_filter(img.astype(np.float), 0)
+	gy = sobel_filter(img.astype(np.float), 1)
 	#
-	gx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=1)
-	gy = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=1)
+	# gx = cv2.Sobel(img, cv2.CV_64F, 1, 0, ksize=1)
+	# gy = cv2.Sobel(img, cv2.CV_64F, 0, 1, ksize=1)
 	#
 
 	mag, angle = cv2.cartToPolar(gx, gy, angleInDegrees=True)
@@ -58,7 +58,7 @@ def gradient(img):
 	return mag, angle
 
 
-def pyramidCreate(image, levels=3):
+def pyramidCreate(image, levelsUp=8, levelsDown=6, scale=0.05):
 	"""
 	image : image matrix
 	levels : quantity of levels in the pyramid
@@ -67,13 +67,18 @@ def pyramidCreate(image, levels=3):
 	pyr = []
 	pyr.append(image)
 
-	for level in range(levels):
-		image = cv2.pyrDown(image)
-		pyr.append(image)
+	levels = [range(1, levelsUp+1), range(-levelsDown, 0)]
+
+	for level in itertools.chain(*levels):
+		newScale = (1+scale*level)
+		newImg = cv2.resize(image, None, fx=newScale, fy=newScale)
+		pyr.append(newImg)
+		# print(level, newImg.shape)
 
 	return pyr
 
 
+@jit(nopython=True)
 def GradientHistogram(mag, angle, cellSz=8, binSz=9):
 	binsShape = (mag.shape[0]//cellSz, mag.shape[1]//cellSz, binSz)
 	bins = np.empty(binsShape)
@@ -87,15 +92,13 @@ def GradientHistogram(mag, angle, cellSz=8, binSz=9):
 									 (bins_x+0)*cellSz: (bins_x+1)*cellSz].flatten()
 
 			# tri-linear histogram
-			hist = [0]*binSz
+			hist = np.zeros((binSz))
 			for angleEl, magEl in zip(cellAngle, cellMag):
 				pos = angleEl/(180/binSz) - 0.5
 				index = int(math.floor(pos)) % binSz
 				weight = abs(pos - int(pos))
 				hist[index] = (1-weight)*magEl
 				hist[(index+1) % binSz] = (weight)*magEl
-
-			hist = np.array(hist)
 
 			# hist, bin_edges = np.histogram(cellAngle, bins=binSz, range=(0, 180), normed=None, weights=cellMag, density=None)
 			
@@ -162,70 +165,116 @@ def extractWindows(image, stride=128, winSz=(128, 64)):
 			yield image[win_y : win_y + winSz[0]  ,  win_x : win_x + winSz[1]]
 
 
-if __name__ == "__main__":
-	total_time = 0
+def extractWindowsRandom(image, windowNo=2, winSz=(128, 64)):
+	image = cv2.copyMakeBorder(
+		image, winSz[0]//4, winSz[0]//4, winSz[1]//4, winSz[1]//4, cv2.BORDER_REPLICATE)
+	
+	start_y = random.randint(0, winSz[0]//4)
+	start_x = random.randint(0, winSz[1]//4)
+	all_y = range(start_y, image.shape[0]-winSz[0], winSz[0])
+	all_x = range(start_x, image.shape[1]-winSz[1], winSz[1])
 
-	images = load.database("/home/html/inf/menotti/ci1028-191/INRIAPerson/test_64x128_H96/pos/")
-	imagesNeg = load.database("/home/html/inf/menotti/ci1028-191/INRIAPerson/Train/neg/")
+	windowNo = min(windowNo, len(all_y), len(all_x))
+
+	while True:
+		try:
+			wins_y = random.sample(all_y, windowNo)
+			wins_x = random.sample(all_x, windowNo)
+			break
+		except ValueError as err:
+			if str(err) == "sample larger than population":
+				# decrease sample
+				print("sample larger than population")
+				windowNo -= 1
+	
+	for win_y, win_x in zip(wins_y, wins_x):
+		yield image[win_y: win_y + winSz[0],  win_x: win_x + winSz[1]]
+			
+
+def extractFeaturesBackground(imagesFalse):
 	x_train = []
 	y_train = []
-
-	for image_arg, image in enumerate(imagesNeg):
-		if image_arg > 200:
-			break
-
-		# start_time = time.time()
-
+	for image_arg, image in enumerate(imagesFalse):
+		# skip some images for faster testing
+		if not image_arg % 12 == 0:
+			continue
+		#
 		image = np.array(image)
-
-		for imageLevel in pyramidCreate(image):
-			for window in extractWindows(imageLevel):
+		for imageLevel in pyramidCreate(image, 8, 6, 0.1):
+			for window in extractWindowsRandom(imageLevel):
+				# show extracted window
 				# cv2.imshow("win", window)
-				# cv2.waitKey(100)
+				# cv2.waitKey(64)
 				features = windowHog(window)
 
 				x_train.append(features)
-				y_train.append(-1)
+				y_train.append(-1)  # negative class
 				if(np.isnan(features).any()):
 					print("- warning: nan features")
 					print("window.shape")
 					print(window.shape)
 					print(features)
 					sys.stdout.flush()
-				
-		# elapsed_time = time.time() - start_time
-		# print("%.5f" % elapsed_time, 'HOG')
-		# total_time += elapsed_time
-	
-	for image_arg, image  in enumerate(images):
+
+	return x_train, y_train
+
+
+def extractFeaturesTrue(imagesTrue):
+	x_train = []
+	y_train = []
+	for image_arg, image in enumerate(imagesTrue):
 		image = np.array(image)
 
-		# start_time = time.time()
-
 		features = windowHog(image)
-
-		# elapsed_time = time.time() - start_time
-		# print(elapsed_time, 'HOG ', image_arg)
-		# total_time += elapsed_time
 
 		x_train.append(features)
 		y_train.append(1)
 
 		# cv2.imshow("image"+str(image_arg), image)
-		# cv2.waitKey(100)
+		# cv2.waitKey(64)
 		# cv2.destroyAllWindows()
+		
+	return x_train, y_train
 
-	clf = svm.SVC(gamma='auto')
+if __name__ == "__main__":
+	total_time = 0
+
+	imagesTrue = load.database(
+		"/home/html/inf/menotti/ci1028-191/INRIAPerson/70X134H96/Test/pos")
+	imagesFalse = load.database("/home/html/inf/menotti/ci1028-191/INRIAPerson/Train/neg/")
+
+	start_time = time.time()
+
+	x_trainFalse, y_trainFalse = extractFeaturesBackground(imagesFalse)
+
+	elapsed_time = time.time() - start_time
+	print("%.5f" % elapsed_time, 'HOG False')
+	total_time += elapsed_time
+	
+	falseNo = len(x_trainFalse)
+
+	print("falseNo")
+	print(falseNo)
+
+	start_time = time.time()
+	
+	x_trainTrue, y_trainTrue = extractFeaturesTrue(imagesTrue)
+
+	elapsed_time = time.time() - start_time
+	print(elapsed_time, 'HOG True')
+	total_time += elapsed_time
+
+	x_train = x_trainFalse + x_trainTrue
+	y_train = y_trainFalse + y_trainTrue
+
+	clf = svm.SVC(C=100, gamma='auto')
+
 	# clf = sklearn.svm.OneClassSVM()
 	# clf.fit(x_train, y_train)
 
-
-	cv_results = model_selection.cross_validate(clf, x_train, y_train, cv=3)
-	print(cv_results)
+	# cv_results = model_selection.cross_validate(clf, x_train, y_train, cv=3)
+	# print(cv_results)
 
 	y_pred = model_selection.cross_val_predict(clf, x_train, y_train, cv=4)
 	conf_mat = metrics.confusion_matrix(y_train, y_pred)
 	print(conf_mat)
-
-	plt.imshow(conf_mat, cmap='binary', interpolation='None')
-	plt.show()
